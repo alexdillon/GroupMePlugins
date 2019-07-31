94e3f81757f8ab63a6694fe06d0be834359e641d
Alex Dillon <alexdillon@knology.net>
2019-07-30 22:53:40 -0500

    Initial commit of MemeEffects and ComonMessages, targetted to Plugin API 1.0.1

---
+++ CommonEffectsPlugin/CommonMessagesPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using GroupMeClientPlugin.MessageCompose;
+
+namespace CommonMessagesPlugin
+{
+    public class CommonMessagesPlugin : GroupMeClientPlugin.PluginBase, IMessageComposePlugin
+    {
+        public string EffectPluginName => "Common Messages";
+
+        public override string PluginDisplayName => "Common Messages Plugin";
+
+        public override string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public Task<MessageSuggestions> ProvideOptions(string typedMessage)
+        {
+            var result = new MessageSuggestions();
+            result.TextOptions.Add(@"¯\_(ツ)_/¯");
+
+            return Task.FromResult<MessageSuggestions>(result);
+        }
+    }
+}
---
+++ MemeEffectsPlugin/MemeEffectsPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using GroupMeClientPlugin.MessageCompose;
+
+namespace MemeEffectsPlugin
+{
+    public class MemeEffectsPlugin : GroupMeClientPlugin.PluginBase, IMessageComposePlugin
+    {
+        public string EffectPluginName => "Meme Effects";
+
+        public override string PluginDisplayName => "Meme Effects Plugin";
+
+        public override string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public Task<MessageSuggestions> ProvideOptions(string typedMessage)
+        {
+            var result = new MessageSuggestions();
+            result.TextOptions.Add(this.DoRandomSpongebobText(typedMessage));
+            result.TextOptions.Add(this.DoAlternatingSpongebobText(typedMessage, true));
+            result.TextOptions.Add(this.DoAlternatingSpongebobText(typedMessage, false));
+
+            return Task.FromResult<MessageSuggestions>(result);
+        }
+
+        private string DoRandomSpongebobText(string text)
+        {
+            Random rnd = new Random();
+            return string.Concat(text.Select(b => (rnd.Next(0,2) == 1 ? b : char.ToUpper(b))));
+        }
+
+        private string DoAlternatingSpongebobText(string text, bool startUpper)
+        {
+            return string.Concat(text.Select(b =>
+            {
+                startUpper = !startUpper;
+                return (startUpper ? b : char.ToUpper(b));
+            }));
+        }
+    }
+}
